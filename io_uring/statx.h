997a5b97bf9f4e6c262076f84c1df53fdb94d121
MahijaMH <151696126+MahijaMH@users.noreply.github.com>
2025-05-14 00:41:03 +0700

    task 4 Update statx.h

--- io_uring/statx.h
+++ io_uring/statx.h
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0
 
+/*
+ * Function: io_statx_prep
+ * Purpose : Prepares a statx operation by extracting necessary parameters
+ *           from the SQE and initializing the corresponding io_statx structure.
+ */
 int io_statx_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe);
+
+/*
+ * Function: io_statx
+ * Purpose : Executes the statx operation using the parameters prepared earlier.
+ *           It invokes the statx system call to retrieve file status information
+ *           and sets the result on the I/O request.
+ */
 int io_statx(struct io_kiocb *req, unsigned int issue_flags);
+
+/*
+ * Function: io_statx_cleanup
+ * Purpose : Cleans up resources allocated for a statx operation.
+ *           This function releases any memory or resource held in the io_statx structure.
+ */
 void io_statx_cleanup(struct io_kiocb *req);
